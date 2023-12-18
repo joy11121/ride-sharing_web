325ed05d730499995f2c8f369099ee6f4fa324d8
Lee Ji Eun <portly0028@desktop.tiffany.com>
2023-12-18 22:56:53 +0800

    create backend branch

--- frontend/src/app/components/MatxLayout/Layout1/Layout1Settings.js
+++
@@ -1,9 +0,0 @@
-const Layout1Settings = {
-  topbar: {
-    show: true,
-    fixed: true,
-    theme: 'whiteBlue' // View all valid theme colors inside MatxTheme/themeColors.js
-  }
-};
-
-export default Layout1Settings;
